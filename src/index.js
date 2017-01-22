1fe2b69dbb84da9eb4af31136251748429d58204
Travis Bloom <travisbloom@gmail.com>
2017-01-22 16:03:34 -0500

    initial scaffold

---
+++ src/index.js
@@ -0,0 +1,27 @@
+// ./src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { AppContainer } from 'react-hot-loader';
+// AppContainer is a necessary wrapper component for HMR
+
+import App from './pages/App/App';
+
+const render = (Component) => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component/>
+    </AppContainer>,
+    document.getElementById('root')
+  );
+};
+
+render(App);
+
+// Hot Module Replacement API
+if (module.hot) {
+  module.hot.accept('./pages/App/App', () => {
+    const NewApp = require('./pages/App/App').default
+    render(NewApp)
+  });
+}
